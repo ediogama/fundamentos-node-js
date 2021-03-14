54926fa9002eb7c3a387415e348956b7de205483
Edio Gama <ediogama@hotmail.com>
2021-03-13 22:53:32 -0300

    add use JSON

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (request, response) => {
     const query = request.query;
     console.log(query);
