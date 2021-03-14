3129394e7ef2293a29a0115455d972ba264de5ad
Edio Gama <ediogama@hotmail.com>
2021-03-13 22:22:17 -0300

    corrigindo erro de escrita da rota

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ app.post("/courses", (request, response) => {
     return response.json({courses: ["Curso 1", "Curso 2", "Curso 3", "Curso 4"]});
 })
 
-app.put("/couses/:id", (request, response) => {
+app.put("/courses/:id", (request, response) => {
     return response.json({courses: ["Curso 6", "Curso 2", "Curso 3", "Curso 4"]});
 })
 
-app.patch("/couses/:id", (request, response) => {
+app.patch("/courses/:id", (request, response) => {
     return response.json({courses: ["Curso 6", "Curso 7", "Curso 3", "Curso 4"]});
 })
 
-app.delete("/couses/:id", (request, response) => {
+app.delete("/courses/:id", (request, response) => {
     return response.json({courses: ["Curso 6", "Curso 2", "Curso 4"]});
 })
 
